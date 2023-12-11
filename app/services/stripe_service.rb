3af50f63927f7d72cfde23a057c00dcbdb706260
Sirbuland Atlas <sirbuland.atlas@gmail.com>
2023-12-11 18:39:37 +0500

    adds stripe service to create and refund charge

---
+++ app/services/stripe_service.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class StripeService
+  def initialize
+    Stripe.api_key = ENV['STRIPE_API_KEY']
+  end
+
+  def create_charge(amount, currency = 'usd')
+    Stripe::Charge.create({
+                            amount:,
+                            currency:,
+                            source: 'tok_visa',
+                            description: 'Charge created with StripeService'
+                          })
+  rescue Stripe::StripeError => e
+    handle_error(e, 'StripeError while creating charge')
+  rescue StandardError => e
+    handle_error(e, 'UnexpectedError while creating charge')
+  end
+
+  def refund_charge(id)
+    Stripe::Refund.create(charge: id)
+  rescue Stripe::StripeError => e
+    handle_error(e, 'StripeError while refunding charge')
+  rescue StandardError => e
+    handle_error(e, 'UnexpectedError while refunding charge')
+  end
+
+  private
+
+  def handle_error(error, message)
+    Rails.logger.error("#{message}: #{error.message}")
+
+    OpenStruct.new(status: 'failed', message: "#{message}: #{error.message}")
+  end
+end
